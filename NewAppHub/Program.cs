f6b2771725f1c5f760eda9e733188ef597ce0f17
Ashish Kamath <kamath901@gmail.com>
2018-08-24 10:02:16 +0530

    working demo signalr with angular

--- NewAppHub/Program.cs
+++ NewAppHub/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace AngularAspNetCoreSignalR
+namespace NewAppHub
 {
     public class Program
     {
--- NewAppHub/Startup.cs
+++ NewAppHub/Startup.cs
@@ -1,19 +1,8 @@
-﻿using AppHub;
-using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Protocols;
-using Microsoft.Owin.Cors;
-using System;
-using System.Configuration;
-using Microsoft.Owin;
-using Owin;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Web.Http;
-namespace AngularAspNetCoreSignalR
+
+namespace NewAppHub
 {
     public class Startup
     {
@@ -42,35 +31,9 @@ namespace AngularAspNetCoreSignalR
 
             app.UseCors("CorsPolicy");
 
-            app.Map("/signalr", map =>
+            app.UseSignalR(routes =>
             {
-                // Setup the CORS middleware to run before SignalR.
-                // By default this will allow all origins. You can 
-                // configure the set of origins and/or http verbs by
-                // providing a cors options with a different policy.
-             //   map.UseCors(CorsOptions.AllowAll);
-
-                if (ConfigurationManager.AppSettings.AllKeys.Contains("TransportConnectTimeout"))
-                    GlobalHost.Configuration.TransportConnectTimeout = TimeSpan.FromSeconds(Convert.ToInt32(ConfigurationManager.AppSettings["TransportConnectTimeout"]));
-
-                if (ConfigurationManager.AppSettings.AllKeys.Contains("ConnectionTimeout"))
-                    GlobalHost.Configuration.ConnectionTimeout = TimeSpan.FromSeconds(Convert.ToInt32(ConfigurationManager.AppSettings["ConnectionTimeout"]));
-
-                GlobalHost.Configuration.KeepAlive = null;
-                var hubConfiguration = new HubConfiguration
-                {
-                    // You can enable JSONP by uncommenting line below.
-                    // JSONP requests are insecure but some older browsers (and some
-                    // versions of IE) require JSONP to work cross domain
-                    EnableJSONP = true,
-                    EnableJavaScriptProxies = true,
-                    EnableDetailedErrors = true
-
-                };
-                // Run the SignalR pipeline. We're not using MapSignalR
-                // since this branch already runs under the "/signalr"
-                // path.
-            //    map.RunSignalR(hubConfiguration);
+                routes.MapHub<MessageHub>("message");
             });
         }
     }
